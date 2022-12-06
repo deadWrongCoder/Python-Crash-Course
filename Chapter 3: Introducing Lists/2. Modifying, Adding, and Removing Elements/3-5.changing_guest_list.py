de7317a50e66cf0f389105439c871e8bcb4003f5
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-12-06 16:21:42 +0300

    Create 3-5.changing_guest_list.py

---
+++ Chapter 3: Introducing Lists/2. Modifying, Adding, and Removing Elements/3-5.changing_guest_list.py
@@ -0,0 +1,9 @@
+guest_list = ["Albert Einstein", "Richard Feynman", "Niels Bohr"]
+print(f"You've been invited to the dinner, {guest_list[0]} ")
+print(f"You've been invited to the dinner, {guest_list[1]} ")
+print(f"You've been invited to the dinner, {guest_list[2]} ")
+print(f"{guest_list[2]} can't make it.")
+guest_list[2] = "Ernest Rutherford"
+print(f"You've been invited to the dinner, {guest_list[0]} ")
+print(f"You've been invited to the dinner, {guest_list[1]} ")
+print(f"You've been invited to the dinner, {guest_list[2]} ")
