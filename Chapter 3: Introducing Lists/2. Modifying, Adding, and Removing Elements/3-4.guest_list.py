b335d210bed2fc8025de06eb85154e22ee3a2a62
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-12-06 16:16:25 +0300

    Create 3-4.guest_list.py

---
+++ Chapter 3: Introducing Lists/2. Modifying, Adding, and Removing Elements/3-4.guest_list.py
@@ -0,0 +1,4 @@
+guest_list = ["Albert Einstein", "Richard Feynman", "Niels Bohr"]
+print(f"You've been invited to the dinner, {guest_list[0]} ")
+print(f"You've been invited to the dinner, {guest_list[1]} ")
+print(f"You've been invited to the dinner, {guest_list[2]} ")
