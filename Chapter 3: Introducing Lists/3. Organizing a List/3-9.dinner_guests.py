7187faef2ed675929e2fe0606aa1c22d7e72789b
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-12-06 18:00:29 +0300

    Create 3-9.dinner_guests.py

---
+++ Chapter 3: Introducing Lists/3. Organizing a List/3-9.dinner_guests.py
@@ -0,0 +1,5 @@
+guest_list = ["Albert Einstein", "Richard Feynman", "Niels Bohr"]
+print(f"You've been invited to the dinner, {guest_list[0]} ")
+print(f"You've been invited to the dinner, {guest_list[1]} ")
+print(f"You've been invited to the dinner, {guest_list[2]} ")
+print(f"{len(guest_list)} people were invited to the dinner.")
