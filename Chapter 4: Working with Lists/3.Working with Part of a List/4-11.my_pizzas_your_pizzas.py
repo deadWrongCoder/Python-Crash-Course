4ae22a13e9b8dfa684112c8be0f863e589f6207b
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-12-07 12:39:15 +0300

    Create 4-11.my_pizzas_your_pizzas.py

---
+++ Chapter 4: Working with Lists/3.Working with Part of a List/4-11.my_pizzas_your_pizzas.py
@@ -0,0 +1,10 @@
+pizzas = ["con carne", 'with cheese', "with chicken"]
+
+friend_pizzas = pizzas[:]
+
+pizzas.append("with pineapple")
+
+friend_pizzas.append("pepperoni")
+
+print(pizzas)
+print(friend_pizzas)
