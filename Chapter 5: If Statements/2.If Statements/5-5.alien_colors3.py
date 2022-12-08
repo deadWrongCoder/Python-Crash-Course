d4f50de59103d3c623c1e5e9603526e78e2e339a
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-12-08 13:58:33 +0300

    Create 5-5.alien_colors3.py

---
+++ Chapter 5: If Statements/2.If Statements/5-5.alien_colors3.py
@@ -0,0 +1,8 @@
+alien_color = "Red"
+
+if alien_color.lower() == "green":
+    print("You've just earned 5 points. ")
+elif alien_color.lower() == "yellow":
+    print("You've just earned 10 points.")
+else:
+    print("You've just earned 15 points.")
