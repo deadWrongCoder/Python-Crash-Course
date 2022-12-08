4f9fd57bec5eb1e2c12d472aaf96acd790a78eff
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-12-08 14:45:49 +0300

    Create 6-1.person.py

---
+++ Chapter 6: Dictionaries/1.Working with Dictionaries/6-1.person.py
@@ -0,0 +1,5 @@
+person = {"name": "Lawrence", "lastname": "Krauss", "age": "68", "country": "USA"}
+print(f"Name: {person['name']}")
+print(f"Last name: {person['lastname']}")
+print(f"Age: {person['age']}")
+print(f"Country: {person['country']}")
