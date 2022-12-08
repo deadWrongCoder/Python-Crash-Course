39d76a352a1f0ecf5b0a08cf6e8744da8ff62221
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-12-08 14:55:06 +0300

    Create 6-3.glossary.py

---
+++ Chapter 6: Dictionaries/1.Working with Dictionaries/6-3.glossary.py
@@ -0,0 +1,10 @@
+programming_concepts = {
+    "integer": "a positive or negative whole number, including 0",
+    "loop": "a sequence of instructions that is continually repeated until a certain condition is reached",
+    "list": "a data type used to store a collection of similar data items",
+    "dictionary": "a key-value store",
+    "string": "a sequence of characters, either as a literal constant"
+}
+
+for key, value in programming_concepts.items():
+    print(f"{key}\n\t{value}")
