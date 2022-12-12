9ab393a49d31a0a824c385673e4dc0ae994d5147
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-12-12 11:01:08 +0300

    Create 7-2.restaurant-seating.py

---
+++ Chapter 7: User Input and While Loops/1.Try it yourself/7-2.restaurant-seating.py
@@ -0,0 +1,5 @@
+people_count = int(input("How many people are in your  dinner group? "))
+if people_count > 8:
+    print("You have to wait for a table. ")
+else:
+    print("Your table is ready. ")
