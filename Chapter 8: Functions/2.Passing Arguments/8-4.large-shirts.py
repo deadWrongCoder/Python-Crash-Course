1fe12b604e23228b9730828908c7bc410b7a0ce1
deadWrongCoder <118064189+deadWrongCoder@users.noreply.github.com>
2022-12-13 15:56:13 +0300

    Create 8-4.large-shirts.py

---
+++ Chapter 8: Functions/2.Passing Arguments/8-4.large-shirts.py
@@ -0,0 +1,6 @@
+def make_shirt(size="L", msg="I love Python"):
+    print(f"You've chosen {size} size T-shirt and printed '{msg}' on it.")
+
+make_shirt()
+make_shirt(size= "M")
+make_shirt(msg="Goodbye World", size="L")
